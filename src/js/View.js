b1dfa570496bbb6ab3d9d51e265417ebf89ae1e0
Adam Riggs <riggs@icscreative.com>
2026-04-26 23:15:00 -0400

    can save and load charts based on uuid

--- src/js/View.js
+++ src/js/View.js
@@ -34,8 +34,14 @@ const chartTypes = [
 ];
 
 export class View {
-	// elements = {};
-	newChart = {};
+	displayChartData = {
+		type: '',
+		data: [],
+		labels: [],
+		id: crypto.randomUUID(),
+	};
+	displayChartObject;
+	savedCharts = [];
 	chartButtons = [];
 
 	constructor() {
@@ -43,87 +49,177 @@ export class View {
 	}
 
 	init() {
-		// console.log('init()');
 		this.initDomElements();
 		this.initChartTypes();
 		this.initListeners();
 
-		// this.drawChart();
+		this.chartButtons[0].click();
+		this.loadSavedCharts();
 	}
 
 	initDomElements() {
-		this.selectChartElem = document.querySelector('.chart-editor__select');
+		this.savedChartsElem = document.querySelector('.chart-list__saved-charts');
+		this.selectChartList = document.querySelector('.chart-editor__select ul');
 		this.data = document.querySelector('.chart-editor__data__input');
-		this.drawBtn = document.querySelector('.chart-editor__preview__draw');
+		// this.drawBtn = document.querySelector('.chart-editor__controls__draw');
+		this.saveBtn = document.querySelector('.chart-editor__controls__save');
+		this.exitBtn = document.querySelector('.chart-editor__controls__exit');
 		this.previewCanvas = document.querySelector('.chart-editor__preview canvas');
 	}
 
 	initChartTypes() {
 		chartTypes.forEach(chart => {
-			// console.log(chart);
-			const btn = document.createElement("button");
+			const li = document.createElement('li');
+			const btn = document.createElement('button');
 			btn.classList.add('chart-editor__select__' + chart);
 			btn.textContent = chart;
 
-			this.selectChartElem.appendChild(btn);
+			btn.dataset.chartType = chart;
 
+			li.appendChild(btn);
+			this.selectChartList.appendChild(li);
 			this.chartButtons.push(btn);
 		});
+	}
+
+	setChartTypeButtons(type) {
+		this.chartButtons.forEach(btn => {
+			btn.classList.remove('selected');
+
+			if (type === btn.dataset.chartType) {
+				btn.classList.add('selected');
+			}
+		})
+		this.selectChart(type);
 	}
 
 	initListeners() {
 		this.chartButtons.forEach(btn => {
 			btn.addEventListener('click', (event) => {
-				this.selectChart(event.target.textContent);
-			})
+				this.setChartTypeButtons(event.target.dataset.chartType);
+			});
 		})
 
 		this.data.addEventListener('input', (event) => {
 			this.dataUpdate(event.target.value);
 		});
 
-		this.drawBtn.addEventListener('click', (() => {
-			this.drawChart();
-		}).bind(this))
+		// this.drawBtn.addEventListener('click', (() => {
+		// 	this.drawChart();
+		// }));
+
+		this.saveBtn.addEventListener('click', (() => {
+			console.log('save');
+			this.saveChart();
+		}));
+	}
+
+	saveChart() {
+		const savedChart = this.getSavedChart(this.displayChartData.id);
+		console.log('displayChartData:', this.displayChartData);
+		console.log('savedChart:', savedChart);
+		if (savedChart === null) {
+			console.log('null');
+			this.savedCharts.push(this.displayChartData);
+		} else {
+			console.log('else');
+			this.savedCharts.forEach(chart => {
+				// console.log(chart);
+				console.log(chart.id);
+				console.log(this.displayChartData.id);
+				console.log('*****');
+				if (chart.id === this.displayChartData.id) {
+					console.log('yep');
+					chart = this.displayChartData;
+				}
+			})
+		}
+		
+		console.log(this.savedCharts);
+		localStorage.setItem('savedCharts', JSON.stringify(this.savedCharts));
+	}
+
+	loadSavedCharts() {
+		console.log('loadSavedCharts()');
+		this.savedCharts = JSON.parse(localStorage.getItem('savedCharts')) || [];
+
+		if (this.savedCharts.length > -1) {
+			this.savedChartsElem.textContent = '';
+
+			this.savedCharts.forEach((chart, i)=> {
+				const li = document.createElement('li');
+				const chartElem = document.createElement('button');
+				chartElem.dataset.chartId = chart.id;
+				chartElem.textContent = chart.type;
+
+				chartElem.addEventListener('click', (event) => {
+					this.loadChart(event.target.dataset.chartId);
+				});
+
+				li.appendChild(chartElem);
+				this.savedChartsElem.appendChild(li);
+			})
+		}
+	}
+
+	loadChart(id) {
+		const displayChartData = this.getSavedChart(id);
+		// console.log(displayChartData);
+		this.displayChartData = displayChartData;
+
+		this.data.textContent = displayChartData.data.join(', ');
+		this.setChartTypeButtons(displayChartData.type);
+	}
+
+	getSavedChart(id) {
+		let returnChart = null;
+		this.savedCharts.forEach(chart => {
+			if (chart.id === id) {
+				returnChart = chart;
+			}
+		});
+		return returnChart;
 	}
 
 	selectChart(type) {
-		// console.log('selectChart(' + type + ')');
-		this.newChart.type = type;
+		this.displayChartData.type = type;
 
-		this.drawChart();
+		if (this.data.value !== '') {
+			this.dataUpdate();
+		}
+
+		if (this.displayChartData.data !== null || this.displayChartData.data.length !== -1) {
+			this.drawChart();
+		}
 	}
 
 	dataUpdate() {
-		// console.log('dataUpdate(' + data + ')');
-		console.log(this.data);
-		this.newChart.data = Array.from(this.data.value.split(',').map(n => parseInt(n.trim(), 10)));
-		this.newChart.labels = this.newChart.data;
-		console.log(this.newChart);
+		this.displayChartData.data = Array.from(this.data.value.split(',').map(n => parseInt(n.trim(), 10)));
+		this.displayChartData.labels = this.displayChartData.data;
+
+		if (this.displayChartData.type !== '') {
+			this.drawChart();
+		}
 	}
 
 	drawChart() {
 		console.log('drawChart()');
-		console.log(this.newChart);
-		this.dataUpdate();
 
-		// const data = Array.from(this.newChart.data.split(',').map(n => parseInt(n.trim(), 10)));
-		// const labels = data;
+		if (this.displayChartData.data === null || this.displayChartData.type === '') {
+			return;
+		}
 
-		console.log('data:', this.newChart.data);
-		console.log('labels:', this.newChart.labels);
+		if (this.displayChartObject) { this.displayChartObject.destroy(); }
 
-		if (this.newChart.chart) { this.newChart.chart.destroy(); }
-
-		this.newChart.chart = new Chart(
+		this.displayChartObject = new Chart(
 			this.previewCanvas,
 			{
-				type: this.newChart.type,
+				type: this.displayChartData.type,
 				data: {
-					labels: this.newChart.labels,
+					labels: this.displayChartData.labels,
 					datasets: [{
 						// label: '# of Votes',
-						data: this.newChart.data,
+						data: this.displayChartData.data,
 						// borderWidth: 1
 					}]
 				},
