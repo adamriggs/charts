089d2edca3d73edeeb33f504b91e93c7058e1429
Adam Riggs <riggs@icscreative.com>
2026-04-27 10:34:17 -0400

    saved charts is functional on a basic level

--- README.md
+++ README.md
@@ -8,10 +8,12 @@ TODO
  * - slide show of steps 
  * - improve the chart list too
  * edit chart labels
+ * separate into mvc
  * multidimensional data
  * save and list charts - save to local storage for now
  * export as png/jpg
  * work with saved datasets - load & manage data from an external file
  * pull data from a live feed
+ * optionally for data sets with multiple points be able to cycle through sets of points automatically
 
 [View Here](https://adamriggs.github.io/charts/)
--- src/js/View.js
+++ src/js/View.js
@@ -11,9 +11,14 @@
  * - better UI and flow
  * - - better buttons
  * - - slide show of steps 
+ * - + new chart button
+ * - - delete chart option
+ * - - CRUID complete
+ * - - edit labels and add title
  * - - improve the chart list too
  * - edit chart labels
  * - multidimensional data
+ * - try catch's around everthing to report errors to the user using dialog boxes
  * - save and list charts - save to local storage for now
  * - export as png/jpg
  * - work with saved datasets - load & manage data from an external file
@@ -34,12 +39,7 @@ const chartTypes = [
 ];
 
 export class View {
-	displayChartData = {
-		type: '',
-		data: [],
-		labels: [],
-		id: crypto.randomUUID(),
-	};
+	displayChartData = this.newChart();
 	displayChartObject;
 	savedCharts = [];
 	chartButtons = [];
@@ -61,7 +61,7 @@ export class View {
 		this.savedChartsElem = document.querySelector('.chart-list__saved-charts');
 		this.selectChartList = document.querySelector('.chart-editor__select ul');
 		this.data = document.querySelector('.chart-editor__data__input');
-		// this.drawBtn = document.querySelector('.chart-editor__controls__draw');
+		this.newBtn = document.querySelector('.chart-editor__new');
 		this.saveBtn = document.querySelector('.chart-editor__controls__save');
 		this.exitBtn = document.querySelector('.chart-editor__controls__exit');
 		this.previewCanvas = document.querySelector('.chart-editor__preview canvas');
@@ -71,18 +71,22 @@ export class View {
 		chartTypes.forEach(chart => {
 			const li = document.createElement('li');
 			const btn = document.createElement('button');
+			btn.classList.add('chart-editor__select');
 			btn.classList.add('chart-editor__select__' + chart);
-			btn.textContent = chart;
-
 			btn.dataset.chartType = chart;
 
+			const div = document.createElement('div');
+			div.classList.add('inner-button');
+			div.textContent = chart;
+
+			btn.appendChild(div);
 			li.appendChild(btn);
 			this.selectChartList.appendChild(li);
 			this.chartButtons.push(btn);
 		});
 	}
 
-	setChartTypeButtons(type) {
+	setChartType(type) {
 		this.chartButtons.forEach(btn => {
 			btn.classList.remove('selected');
 
@@ -96,7 +100,8 @@ export class View {
 	initListeners() {
 		this.chartButtons.forEach(btn => {
 			btn.addEventListener('click', (event) => {
-				this.setChartTypeButtons(event.target.dataset.chartType);
+				console.log(event.target);
+				this.setChartType(event.target.closest('.chart-editor__select').dataset.chartType);
 			});
 		})
 
@@ -104,39 +109,54 @@ export class View {
 			this.dataUpdate(event.target.value);
 		});
 
-		// this.drawBtn.addEventListener('click', (() => {
-		// 	this.drawChart();
-		// }));
+		this.newBtn.addEventListener('click', (() => {
+			this.displayChartData = this.newChart();
+			// console.log('this.displayChartData:', this.displayChartData);
+
+			this.drawChart();
+		}));
 
 		this.saveBtn.addEventListener('click', (() => {
-			console.log('save');
 			this.saveChart();
 		}));
 	}
 
+	newChart() {
+		const newChart = {
+			type: '',
+			data: [],
+			labels: [],
+			id: crypto.randomUUID(),
+		};
+
+		if (this.data) {
+			this.data.textContent = '';
+		}
+
+		if (this.chartButtons && this.chartButtons.length >= 0) {
+			this.chartButtons.forEach(btn => {
+				btn.classList.remove('selected');
+			})
+		}
+
+		return newChart;
+	}
+
 	saveChart() {
 		const savedChart = this.getSavedChart(this.displayChartData.id);
-		console.log('displayChartData:', this.displayChartData);
-		console.log('savedChart:', savedChart);
 		if (savedChart === null) {
-			console.log('null');
 			this.savedCharts.push(this.displayChartData);
 		} else {
-			console.log('else');
 			this.savedCharts.forEach(chart => {
-				// console.log(chart);
-				console.log(chart.id);
-				console.log(this.displayChartData.id);
-				console.log('*****');
 				if (chart.id === this.displayChartData.id) {
-					console.log('yep');
 					chart = this.displayChartData;
 				}
 			})
 		}
 		
-		console.log(this.savedCharts);
 		localStorage.setItem('savedCharts', JSON.stringify(this.savedCharts));
+
+		this.loadSavedCharts();
 	}
 
 	loadSavedCharts() {
@@ -148,15 +168,41 @@ export class View {
 
 			this.savedCharts.forEach((chart, i)=> {
 				const li = document.createElement('li');
-				const chartElem = document.createElement('button');
-				chartElem.dataset.chartId = chart.id;
-				chartElem.textContent = chart.type;
 
-				chartElem.addEventListener('click', (event) => {
-					this.loadChart(event.target.dataset.chartId);
+				const btn = document.createElement('button');
+				btn.classList.add('saved-chart');
+				btn.dataset.chartId = chart.id;
+
+				const div = document.createElement('div');
+				div.classList.add('inner-button');
+
+				const title = document.createElement('span');
+				title.classList.add('inner-button__title');
+				title.textContent = chart.type;
+
+				const preview = document.createElement('span');
+				preview.classList.add('inner-button__preview');
+
+				const deleteBtn = document.createElement('button');
+				deleteBtn.classList.add('inner-button__delete');
+				deleteBtn.textContent = 'Delete';
+
+				div.appendChild(title);
+				div.appendChild(preview);
+				div.appendChild(deleteBtn);
+				btn.appendChild(div);
+				li.appendChild(btn);
+
+				btn.addEventListener('click', (event) => {
+					this.loadChart(event.target.closest('.saved-chart').dataset.chartId);
 				});
 
-				li.appendChild(chartElem);
+				deleteBtn.addEventListener('click', (event) => {
+					event.preventDefault();
+					event.stopPropagation();
+					this.deleteChart(event);
+				})
+
 				this.savedChartsElem.appendChild(li);
 			})
 		}
@@ -168,7 +214,32 @@ export class View {
 		this.displayChartData = displayChartData;
 
 		this.data.textContent = displayChartData.data.join(', ');
-		this.setChartTypeButtons(displayChartData.type);
+		this.setChartType(displayChartData.type);
+	}
+
+	deleteChart(event) {
+		console.log('deleteChart()');
+		const deleteElem = event.target.closest('.saved-chart');
+		const deleteId = deleteElem.dataset.chartId;
+		console.log(deleteId);
+
+		this.savedCharts.forEach((chart, i) => {
+			let found = -1;
+
+			console.log('deleteId:', deleteId);
+			console.log('chart.id:', chart.id);
+			console.log('*****');
+			if (chart.id === deleteId) {
+				found = i;
+			}
+
+			if (found !== -1) {
+				this.savedCharts.splice(found, 1);
+				localStorage.setItem('savedCharts', JSON.stringify(this.savedCharts));
+			}
+		})
+		this.loadSavedCharts();
+
 	}
 
 	getSavedChart(id) {
@@ -203,13 +274,36 @@ export class View {
 	}
 
 	drawChart() {
-		console.log('drawChart()');
+		// console.log('drawChart()');
 
-		if (this.displayChartData.data === null || this.displayChartData.type === '') {
+		if (!this.displayChartData || 
+			this.displayChartData.data === null ||
+			this.displayChartData.type === '' ||
+			this.displayChartData.data.length <=0
+		) {
+			console.log('no chart data');
+			if (this.displayChartObject) {
+				console.log('no display chart object');
+				this.displayChartObject.destroy();
+			}
+			
+			console.log(this.previewCanvas);
+
+			if (this.previewCanvas) {
+				console.log('display preview canvas exists');
+				console.log('this.previewCanvas:', this.previewCanvas);
+				// this.previewCanvas.clearRect(0, 0, this.previewCanvas.width, this.previewCanvas.height);
+			}
+			console.log('*****');
+
 			return;
 		}
 
 		if (this.displayChartObject) { this.displayChartObject.destroy(); }
+
+		console.log('this.displayChartData:', this.displayChartData);
+		console.log(this.previewCanvas);
+		console.log('*****');
 
 		this.displayChartObject = new Chart(
 			this.previewCanvas,
